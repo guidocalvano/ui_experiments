65aadafc19077ad1e7496503a50aacc4205a5e1e
Guido Sales Calvano <g.salescalvano@mindef.nl>
2025-06-11 12:56:39 +0200

    signal code

--- signals.py
+++ signals.py
@@ -3,15 +3,16 @@ class Signal:
     def __init__(self):
         self.state = None
         self.observers = []
+        self.is_initialized = False
 
         self.is_outdated = True
 
-    def set_observers_outdated(self):
+    def set_outdated(self):
         if self.is_outdated: return
 
         self.is_outdated = True
 
-        [observer.set_observers_outdated() for observer in self.observers]
+        [observer.set_outdated() for observer in self.observers]
 
     def add_observer(self, observer):
         self.observers.append(observer)
@@ -23,8 +24,20 @@ class Signal:
         # underscore to mark as private. This indicates that only input signals should be used to set state
         self.state = state
         self.is_outdated = False
+        # self.is_initialized = True
+        self.propagate_initialized(True)
+        [observer.set_outdated() for observer in self.observers]
 
-        [observer.set_observers_outdated() for observer in self.observers]
+    def propagate_initialized(self, is_initialized):
+        old_is_initialized = self.is_initialized
+
+        self.is_initialized = is_initialized
+
+        if old_is_initialized == is_initialized:
+            return
+
+        [observer.propagate_initialized(self.is_initialized) for observer in self.observers]
+
 
     def get_state(self):
         raise Exception("State cannot be retrieved without defining how")
@@ -42,6 +55,7 @@ class Input(Signal):
 
     def get_state(self):
         return self.state
+
 
 
 class Calculation(Signal):
@@ -64,6 +78,78 @@ class Calculation(Signal):
 
         return self.state
 
+    def propagate_initialized(self, new_is_initialized):
+
+        if not new_is_initialized:
+            self.is_initialized = new_is_initialized
+            [observer.propagate_initialized(self.is_initialized) for observer in self.observers]
+
+        # new_is_initialized is true and calculation was already is_initialized so nothing changes
+        if self.is_initialized:
+            return
+
+        # we are trying to switch on is_initialized, but we need to check if the inputs warrent this
+        # suppose true
+        for input in self.inputs:
+            if not input.is_initialized:
+                return
+
+        self.is_initialized = new_is_initialized
+
+        [observer.propagate_initialized(self.is_initialized) for observer in self.observers]
+
+
+
+class SignalInput(Calculation):
+    def __init__(self, initial_signal=None):
+        if initial_signal is None:
+            initial_signal = Input()
+
+        super().__init__(lambda x: x, [initial_signal])
+
+        if initial_signal is None:
+            self.propagate_initialized(False)
+            return
+
+        self.propagate_initialized(initial_signal.is_initialized)
+
+    def set_input(self, signal):
+        [input.remove_observer(self) for input in self.inputs]
+        signal.add_observer(self)
+
+        self.inputs = [signal]
+
+        self.propagate_initialized(signal.is_initialized)
+        self.set_outdated()
+
+class Output(Calculation):
+
+    def __init__(self, effect=lambda state: None, signal=None):
+        super().__init__(effect, [signal])
+        # to not defer initialisation call get_state immediately.
+        # self.get_state()
+        if signal.is_initialized:
+            self.get_state()
+
+    def propagate_initialized(self, new_is_initialized):
+        if not self.is_initialized and new_is_initialized:
+            self.get_state()
+
+
+
+    def set_outdated(self):
+        super().set_outdated()
+        self.get_state()
+        # self.state = self.inputs[0].get_state()
+        #
+        #
+        # # self.is_outdated = False
+        # #
+        # self.calculation(self.state)
+
+
+
+
 class SignalList(Signal):
     def __init__(self, list):
         super().__init__()
@@ -75,38 +161,12 @@ class SignalList(Signal):
         signal.add_observer(self)
         self.state.append(signal)
 
-        self.set_observers_outdated()
+        self.set_outdated()
 
     def remove(self, signal):
         self.state.remove(signal)
 
-        self.set_observers_outdated()
-
-
-class Output(Calculation):
-
-    def __init__(self, effect=lambda state: None, signal=None):
-        super().__init__(effect, [signal])
-
-        self.in_update = False
-        self.is_outdated = False
-        # self._set_state(signal.get_state())
-        #
-        # self.calculation(self.state)
-
-
-    def set_observers_outdated(self):
-        # super().set_observers_outdated()
-        if self.in_update:
-            return
-        self.in_update = True
-        self.state = self.inputs[0].get_state()
-
-        self.in_update = False
-
-        # self.is_outdated = False
-        #
-        self.calculation(self.state)
+        self.set_outdated()
 
 
 def test():
@@ -141,6 +201,55 @@ def test():
     assert bla[1] == "calc on " + str(5)
     assert calc.state == "calc on " + str(5)
 
+
+    # test deferred initialization
+    ladida = []
+
+    inp2 = Input()
+
+    outp2 = Output(lambda state: ladida.append(state), inp2)
+
+    assert len(ladida) == 0
+
+    inp2.set_state(6)
+    assert ladida[0] == 6
+
+    # SIGNAL INPUT
+    tralala = []
+
+    # test empty initialization
+    si = SignalInput()
+
+    outp3 = Output(lambda state: tralala.append(state), si)
+
+    assert len(tralala) == 0
+    assert si.is_initialized == False
+
+    # test initialization with input that is not initialized
+    inp3 = Input()
+
+    si.set_input(inp3)
+    assert len(tralala) == 0
+    # test then initializing
+    inp3.set_state(7)
+
+    assert inp3.is_initialized == True
+    assert si.is_initialized == True
+    assert outp3.is_initialized == True
+
+    assert tralala[0] == 7
+
+    # test get state
+    c = Calculation(lambda val: val + 2, [si])
+
+    assert len(tralala) == 1
+    assert c.state == None
+
+    assert c.get_state() == 9
+
+    # test attaching output to not initialized signal input
+    # test attaching output to initialized signal input
+    # test swapping initialized input with not initialized input
 
 
 if __name__ == '__main__':
